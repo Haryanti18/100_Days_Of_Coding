8b380657935978bf801358124a114573ae81571c
Haryanti <haryantitahir33@gmail.com>
2023-10-12 11:25:47 +0800

    Day 3

---
+++ Day_3.java
@@ -0,0 +1,12 @@
+package Coding;
+
+public class day_3 {
+
+    // Program sederhana untuk menampilkan "Hello, world!"
+    public static void main(String[] args) {
+
+        // Menampilkan pesan "Hello, world!"
+        System.out.println("Hello, world!");
+    }
+
+}
