edd6522ed0e3f25791ec5f21e67e0ab3adabe127
Haryanti <haryantitahir33@gmail.com>
2023-10-15 06:56:27 +0800

    Day 5

---
+++ Day_5.java
@@ -0,0 +1,16 @@
+package Coding;
+
+public class day_5 {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 10;
+        int b = 20;
+
+        // Penjumlahan
+        int c = a + b;
+
+        // Tampilkan hasil
+        System.out.println("Hasil penjumlahan: " + c);
+    }
+}
