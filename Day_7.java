72062bc4356684f8f674b9e850c81744cc56500c
Haryanti <haryantitahir33@gmail.com>
2023-10-16 06:26:19 +0800

    Day_7

---
+++ Day_7.java
@@ -0,0 +1,38 @@
+package Coding;
+
+public class day_7 {
+      public static void main(String[] args) {
+
+        // Tipe data byte
+        byte byteNumber = 100;
+        System.out.println("Nilai byte: " + byteNumber);
+
+        // Tipe data short
+        short shortNumber = 1000;
+        System.out.println("Nilai short: " + shortNumber);
+
+        // Tipe data int
+        int intNumber = 10000;
+        System.out.println("Nilai int: " + intNumber);
+
+        // Tipe data long
+        long longNumber = 1000000;
+        System.out.println("Nilai long: " + longNumber);
+
+        // Tipe data float
+        float floatNumber = 10.5f;
+        System.out.println("Nilai float: " + floatNumber);
+
+        // Tipe data double
+        double doubleNumber = 10.5;
+        System.out.println("Nilai double: " + doubleNumber);
+
+        // Tipe data boolean
+        boolean booleanValue = true;
+        System.out.println("Nilai boolean: " + booleanValue);
+
+        // Tipe data char
+        char charValue = 'a';
+        System.out.println("Nilai char: " + charValue);
+    }
+}
