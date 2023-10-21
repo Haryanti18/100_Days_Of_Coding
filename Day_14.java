2360bd338572cffb2aa7800fec0a2f26ea011586
Haryanti <haryantitahir33@gmail.com>
2023-10-21 22:02:31 +0800

    Day 14 Tipe Data Non-Primitif : String, Integer dan Boolean

---
+++ Day_14.java
@@ -0,0 +1,19 @@
+package Coding;
+
+public class day_14 {
+    
+    public static void main(String[] args) {
+        // Deklarasi variabel non primitif
+        String nama = "Haryanti";
+        int umur = 21;
+        boolean status = true;
+
+        // Tampilkan nilai variabel
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Status Mahasiswa: " + status);
+
+    }
+}
+
+
