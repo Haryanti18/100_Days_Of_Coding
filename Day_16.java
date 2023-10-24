400c192ecabde69bdbaa9412e2ff48ffb8fb5227
Haryanti <haryantitahir33@gmail.com>
2023-10-24 13:57:02 +0800

    Day 16 Penugasan

---
+++ Day_16.java
@@ -0,0 +1,24 @@
+package Coding;
+
+public class day_16 {
+   public static void main(String[] args) {
+
+        // Deklarasi variabel
+        int alas = 10;
+        int tinggi = 5;
+        int panjang = 20;
+        int lebar = 20;
+        
+        
+        // Menghitung luas segitiga
+        int luas = alas * tinggi /2;
+        
+        // Mengitung luas persegi
+        int luas1 = panjang * lebar;
+
+ 
+        // Menampilkan luas persegi panjang
+        System.out.println("Luas segitiga adalah: " + luas);
+        System.out.println("Luas persegi panjang adalah: " + luas1);
+    }  
+}
