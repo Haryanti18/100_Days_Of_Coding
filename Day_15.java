b89431c3f3ce3d20e8822cef3fbb7be4f7641c5e
Haryanti <haryantitahir33@gmail.com>
2023-10-24 13:54:44 +0800

    Day 15 Aritmatika/Matematika

---
+++ Day_15.java
@@ -0,0 +1,36 @@
+package Coding;
+
+public class day_15 {
+    public static void main(String[] args) {
+ 
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c); 
+
+        // Pengurangan
+        int d = 20;
+        int e = 10;
+        int f = d - e;
+        System.out.println(f); 
+
+        // Perkalian
+        int g = 10;
+        int h = 20;
+        int i = g * h;
+        System.out.println(i); 
+
+        // Pembagian
+        int j = 20;
+        int k = 10;
+        int l = j / k;
+        System.out.println(l); 
+
+        // Modulo
+        int m = 20;
+        int n = 10;
+        int o = m % n;
+        System.out.println(o); 
+    }
+}
+
