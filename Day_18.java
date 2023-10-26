0c559236c94c43e598b66e6c1eb11b05ec47ad06
Haryanti <haryantitahir33@gmail.com>
2023-10-26 16:58:21 +0800

    Day 18 Percabangan

---
+++ Day_18.java
@@ -0,0 +1,15 @@
+package Coding;
+
+public class day_18 {
+   public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilai = 70;
+
+        // Percabangan if
+        if (nilai >= 75) {
+            System.out.println("Selamat, Anda lulus ujian!");
+        } else {
+            System.out.println("Anda tidak lulus ujian.");
+        }
+    } 
+}
