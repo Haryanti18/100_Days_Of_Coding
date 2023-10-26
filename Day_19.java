4873d7b79b80713b6c53ee9b2fbe25de7f0f7aa1
Haryanti <haryantitahir33@gmail.com>
2023-10-26 17:00:29 +0800

    Day 19 Percabangan

---
+++ Day_19.java
@@ -0,0 +1,19 @@
+package Coding;
+
+public class Day_19 {
+     public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilai = 85;
+
+        // Percabangan if else
+        if (nilai >= 90) {
+            System.out.println("Nilai anda A.");
+        } else if (nilai >= 80) {
+            System.out.println("Nilai anda B.");
+        } else if (nilai >= 70) {
+            System.out.println("Nilai anda C.");
+        } else {
+            System.out.println("Nilai anda D atau E.");
+        }
+    }
+}
