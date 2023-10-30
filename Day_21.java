c65c7bda0b1eceaef052d01f10ccc0df2a5158e4
Haryanti <haryantitahir33@gmail.com>
2023-10-30 21:11:35 +0800

    Day 21 Percabangan

---
+++ Day_21.java
@@ -0,0 +1,14 @@
+package Coding;
+
+public class day_21 {
+     public static void main(String[] args) {
+       
+        int angka = 10;
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " adalah bilangan genap.");
+        } else {
+            System.out.println("Angka " + angka + " adalah bilangan ganjil.");
+        }
+    }
+}
