7cd4bbc7bfcadd2c29a5011a81a967b09f653fd0
Haryanti <haryantitahir33@gmail.com>
2023-10-30 21:08:07 +0800

    Day 20 Percabangan

---
+++ Day_20.java
@@ -0,0 +1,25 @@
+package Coding;
+
+public class day_20 {
+
+    public static void main(String[] args) {
+        int hari = 2;
+
+        if (hari == 1) {
+            System.out.println("Hari ini adalah hari Minggu.");
+        } else if (hari == 2) {
+            System.out.println("Hari ini adalah hari Senin.");
+        } else if (hari == 3) {
+            System.out.println("Hari ini adalah hari Selasa.");
+        } else if (hari == 4) {
+            System.out.println("Hari ini adalah hari Rabu.");
+        } else if (hari == 5) {
+            System.out.println("Hari ini adalah hari Kamis.");
+        } else if (hari == 6) {
+            System.out.println("Hari ini adalah hari Jumat.");
+        } else {
+            System.out.println("Hari ini adalah hari Sabtu.");
+        }
+    }
+}
+
