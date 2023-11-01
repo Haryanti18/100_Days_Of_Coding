4e364b5a3d507b6eaeeb96d63395b58e87ecc649
Haryanti <haryantitahir33@gmail.com>
2023-11-01 20:14:20 +0800

    Day 25 Perulangan For

---
+++ Day_25.java
@@ -0,0 +1,13 @@
+package Coding;
+
+public class day_25 {
+
+    public static void main(String[] args) {
+        System.out.println("Angka genap dari 1 hingga 10:");
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
