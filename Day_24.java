a7cc5bacbb49f2513dd23a18c888f4d805720bbc
Haryanti <haryantitahir33@gmail.com>
2023-11-01 20:10:20 +0800

    Day 24 Perulangan For

---
+++ Day_24.java
@@ -0,0 +1,9 @@
+package Coding;
+
+public class day_24 {
+     public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Angka: " + i);
+        }
+    }
+}
