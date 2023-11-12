4313027decd889f497a4478d5bc26077a28f4534
Haryanti <haryantitahir33@gmail.com>
2023-11-13 07:35:05 +0800

    Day 34 Perulangan for yang mencetak angkka yang habis di bagi 3

---
+++ Day_34.java
@@ -0,0 +1,11 @@
+package Coding;
+
+public class day_34 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
