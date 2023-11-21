ec5f0564c0078d559c7cfb07fbfe641de4598805
Haryanti <haryantitahir33@gmail.com>
2023-11-21 22:11:22 +0800

    Day 44 membuat sebuah inputan dengan memasukkan nama dana akan mencetak sebuah kketerangan

---
+++ Day_44.java
@@ -0,0 +1,13 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class day_44 {
+    public static void main(String[] args) {
+        Scanner an = new Scanner (System.in);
+        System.out.print("Masukkan nama anda : ");
+        String nama = an.nextLine();
+        System.out.print(nama+" Sedang Melakukan Proses 100 Days Of Ngoding ");
+    }
+    
+}
