cca0360d72491c956910578325198ce4e1646102
Haryanti <haryantitahir33@gmail.com>
2023-11-24 23:11:18 +0800

    Day 46 menampilkan array yang menampung nama buah

---
+++ Day_46.java
@@ -0,0 +1,16 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class day_46 {
+  public static void main(String[] args) {
+
+        // Deklarasi variabel
+        String[] namaBuah = {"Apel", "Mangga", "Jeruk", "Pisang", "Anggur"};
+
+        // Tampilkan nama buah
+        for (String nama : namaBuah) {
+            System.out.println(nama);
+        }
+    }
+}
