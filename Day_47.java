59c8096a0777ce5ece4026072ae008f6e3ed3a1c
Haryanti <haryantitahir33@gmail.com>
2023-11-24 23:14:16 +0800

    Day 47 menampilkan array yang menampung nama hewan

---
+++ Day_47.java
@@ -0,0 +1,13 @@
+package Coding;
+
+public class day_47 {
+     public static void main(String[] args) {
+       
+        String[] namaHewan = {"Kucing", "Anjing", "Kuda", "Sapi", "Kelinci"};
+
+        
+        for (String nama : namaHewan) {
+            System.out.println(nama);
+        }
+    }
+}
